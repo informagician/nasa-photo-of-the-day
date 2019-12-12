4fc5b7bc87b1edc83b4705f3454c5d0d1c65d222
Milo Rastgoo <hello@informagician.com>
2019-12-12 15:30:46 -0500

    initial commit day 2

--- src/components/main.js
+++ src/components/main.js
@@ -3,14 +3,16 @@ import React, { useState, useEffect } from "react";
 const axios = require('axios');
 
 function Main(props) {
-    const [apod, setApod] = useState([])
-    var [date, setDate] = useState((new Date()).toISOString().split('T')[0])
-    var [dayCount, setDayCount] = useState(0)
+    const [apod, setApod] = useState([]);
+    var [date, setDate] = useState((new Date()).toISOString().split('T')[0]);
+    var [dayCount, setDayCount] = useState(0);
 
     var today = (new Date()).toISOString().split('T')[0];
-    var dayBefore = (new Date(Date.now() - (Math.abs(dayCount) * 86400000))).toISOString().split('T')[0];
-    var dayAfter = (new Date(Date.now() - (Math.abs(dayCount) * 86400000))).toISOString().split('T')[0];
+    var dayBefore = (new Date(Date.now() - (Math.abs(dayCount-1) * 86400000))).toISOString().split('T')[0];
+    var dayAfter = (new Date(Date.now() - (Math.abs(dayCount+1) * 86400000))).toISOString().split('T')[0];
     var currentDate = date;
+    var test = (new Date()).toString().split('T')[0];
+    console.log(test);
 
     useEffect(() => {
         axios.get(`https://api.nasa.gov/planetary/apod?api_key=IxChViHyyrlKLnnbDRB8mQt16hd8f22mYI9TefeR&date=${date}`)
@@ -27,14 +29,14 @@ function Main(props) {
     } else {
     return (
         <main>
-            <button onClick={() => {setDate(dayBefore);setDayCount(dayCount-1)}}>Previous Day</button>
+            <button onClick={() => {setDate(dayBefore);setDayCount(--dayCount)}}>Previous Day</button>
             <button onClick={() => {setDate(today);setDayCount(0)}}>Today</button>
-            <button onClick={() => {setDate(dayAfter);setDayCount(dayCount+1)}} disabled={(today == currentDate) ? true : false}>Next Day</button>
+            <button onClick={() => {setDate(dayAfter);setDayCount(++dayCount)}} disabled={(today === currentDate) ? true : false}>Next Day</button>
             <h1>{apod.title}</h1>
             <p>Date: {apod.date}</p>
             <p>{apod.explanation}</p>
-            <img src={apod.url} alt={apod.title} style={{display: apod.media_type == 'image' ? 'block' : 'none'}} />
-            <iframe src={apod.url} alt={apod.title} style={{display: apod.media_type == 'video' ? 'block' : 'none'}}></iframe>
+            <img src={apod.url} alt={apod.title} style={{display: apod.media_type === 'image' ? 'block' : 'none'}} />
+            <iframe src={apod.url} title={apod.title} style={{display: apod.media_type === 'video' ? 'block' : 'none'}}></iframe>
         </main>
     );
     }
